3184cb21b9cba35bb77519691813c81bf74760d1
Mudassir <this.is.mudassir.204@gmail.com>
2026-06-13 03:01:56 +0500

    fix: server action error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
 
   // Performance optimizations
   poweredByHeader: false,
@@ -95,7 +92,6 @@ const nextConfig = {
   },
 
   experimental: {
-    serverActions: true,
     optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
   },
 };
